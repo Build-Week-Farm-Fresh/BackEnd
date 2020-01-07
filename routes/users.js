05c2d098ae420ca093bd6a75d2bf7b373b878e59
Zach Taylor <zbtaylor1@gmail.com>
2020-01-06 21:30:41 -0500

    user model updated

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Update User
+router.put("/:id", (req, res) => {
+  Users.update(req.params.id, req.body)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      // handle error
+    });
+});
+
+// Delete User
+router.delete("/:id", (req, res) => {
+  Users.remove(req.params.id)
+    .then(removed => {
+      res.status(200).json(removed);
+    })
+    .catch(err => {
+      // handle error
+    });
 });
 
 module.exports = router;
